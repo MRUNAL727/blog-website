65d0b83cafff146f216d5c5a178eec48914887d1
Mrunal <mrunal4271@gmail.com>
2022-02-15 09:49:40 +0530

    second

--- index.js
+++ index.js
@@ -60,9 +60,12 @@ const storage = multer.diskStorage({
   });
 
   
-  // if(process.env.NODE_ENV= 'production'){
-  //   app.use(express.static("client/build"))
-  // }
+  if(process.env.NODE_ENV= 'production'){
+    app.use(express.static("client/build"))
+     app.get("*", (req,res)=>{
+      res.sendFile(path.resolve(__dirname, 'client', "build", "index.html"))
+    })
+  }
 
   // app.get('/', (req, res)=>{
   //   res.send('Hello')
