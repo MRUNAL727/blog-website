8333d4c350a9feb68ab16a907058376331b2575d
Mrunal <mrunal4271@gmail.com>
2022-02-15 10:25:11 +0530

    second

--- index.js
+++ index.js
@@ -61,10 +61,10 @@ const storage = multer.diskStorage({
 
   
   if(process.env.NODE_ENV= 'production'){
-    app.use(express.static("client/build"))
+    app.use(express.static(path.join( __dirname ,"/client/build")))
 
      app.get("*", (req,response)=>{
-      response.sendFile(path.resolve(__dirname, 'client', "build/index.html"))
+      response.sendFile(path.resolve(__dirname, 'client', "build", "index.html"))
     })
   } 
 
