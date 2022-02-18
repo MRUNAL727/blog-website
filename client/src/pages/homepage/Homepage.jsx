d290d1602224ac69b9b5f32283952ec08e890ee1
Mrunal <mrunal4271@gmail.com>
2022-02-18 09:40:17 +0530

    changes

--- client/src/pages/homepage/Homepage.jsx
+++ client/src/pages/homepage/Homepage.jsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const res = await axios.get("http://localhost:5000/api/posts" + search);
+      const res = await axios.get("https://asdjbaksjdbkab.herokuapp.com/api/posts" + search);
       console.log(res)
       setPosts(res.data);
     };
