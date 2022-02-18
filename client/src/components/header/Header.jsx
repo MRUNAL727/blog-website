30ca1060d55572afbeb56bef9ef49026473f2b8f
Mrunal <mrunal4271@gmail.com>
2022-02-18 13:03:38 +0530

    second

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -2,16 +2,21 @@ import "./header.css";
 
 export default function Header() {
   return (
-    <div className="header">
-      {/* <div className="headerTitles">
-        <span className="headerTitleSm">React & Node</span>
-        <span className="headerTitleLg">BLOG</span>
-      </div> */}
-      <img
-        className="headerImg"
-        src="./images/helloworld.jpg"
-        alt=""
-      />
-    </div>
+    // <div className="header">
+    //   {/* <div className="headerTitles">
+    //     <span className="headerTitleSm">React & Node</span>
+    //     <span className="headerTitleLg">BLOG</span>
+    //   </div> */}
+    //   {/* <img
+    //     className="headerImg"
+    //     src="./images/helloworld.jpg"
+    //     alt=""
+    //   /> */}
+    // </div>
+    <>
+      <div className="head">
+        <h1 className="helloWorld">Hello World</h1>
+      </div>
+    </>
   );
 }
